614434dd535063f4d8e43c8fbd877755e5d41efa
Dayna Clare <daycanimate@gmail.com>
2025-06-02 13:38:53 -0500

    connected endpoint to database and updated template

--- app/controllers/error_controller.py
+++ app/controllers/error_controller.py
@@ -1,5 +1,7 @@
 from app import app
 from flask import render_template
+
+
 @app.errorhandler(404)
 def page_not_found(error):
     """Custom 404 page."""
--- app/tests/test_templates.py
+++ app/tests/test_templates.py
@@ -1,11 +1,60 @@
-from app import app, db,login_manager
-from flask import render_template, request, redirect, url_for, flash,jsonify,session
+from app import app, db, login_manager
+from flask import Flask, render_template, request, redirect, url_for, flash, make_response, jsonify, session
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required
+
+from app.models.user import User
+from app.services.reccommendation_service import RecommendationService
+
+from app.models.user_activity_preferences import UserActivityPreference
+from app.models.categories import Category
+from app.models.airport import Airport
+from app.models.country import Country
+from app.models.city import City
+from app.models.hotel import Hotel
+from app.models.activity import Activity
 
 
 @app.route('/selection-test')
 def selection_test():
+    """Render website's preference selection page."""    
+    
+    categories = []
+    
+    try:
+        categories_result = db.session.execute(db.select(Category)).scalars().all()
+        #
+        
+        if categories_result: 
+            for category in categories_result:
+                activities = []
+                
+                try: 
+                    activities_result = db.session.execute(db.select(Activity).filter_by(category=category.name)).scalars().all()
+                     
+                    if activities_result:
+                        activities = activities_result
+                        
+                except Exception as e:
+                    return make_response(jsonify({"error": str(e)}), 500)
+                
+                print({"category_name": category.name, "category_id": category.id, "category_activities": activities})
+                
+                categories.append({"category_name": category.name, "category_id": category.id, "category_activities": activities})
+        
+        else:
+            return make_response(jsonify({"error": "User not found"}), 404)
+        
+
+    except Exception as e:
+        return make_response(jsonify({"error": str(e)}), 500)
+    
+    finally:
+        return render_template('selection_pg/selection_base.html', categories=categories)
+
+@app.route('/recommendations-test')
+def recommendations_test():
     """Render website's preference selection page."""
-    return render_template('selection_pg/selection_base.html')
+    return render_template('destinations_pg/destinations.html')
 
 @app.route('/clear-session')
 def clear_session():
